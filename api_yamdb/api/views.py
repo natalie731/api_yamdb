5ed35fe7ae7c5efd4476d91ce175ae8aed3823ab
Наталья Банникова <89516835720@ya.ru>
2022-08-09 21:10:48 +0300

    New branch

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,5 @@
 #from base64 import encode
-#from django.core.mail import send_mail
+from django.core.mail import send_mail
 from rest_framework import mixins, viewsets
 
 from users.models import User
@@ -19,14 +19,14 @@ class UserViewSet(CreateUserViewSet, viewsets.GenericViewSet):
         #    confirmation_code = encode('')
         #except Exception('')
         serializer.save()
-        #try:
-        #    send_mail(
-        #        'Подтверждение регистрации на проекте YaMDB',
-        #        f'Пройдите по ссылке http/api/v1/auth/token
-        #          Введите этот код проверки {confirmation_code}',
-        #        'from@example.com',
-        #        ['to@example.com'],
-        #        fail_silently=False, # Сообщать об ошибках («молчать ли об ошибках?»)
-        #    )
-        #except Exception('')
+        try:
+            send_mail(
+                'Подтверждение регистрации на проекте YaMDB',
+                f'Пройдите по ссылке http/api/v1/auth/token
+                  Введите этот код проверки {confirmation_code}',
+                'from@example.com',
+                ['to@example.com'],
+                fail_silently=False, # Сообщать об ошибках («молчать ли об ошибках?»)
+            )
+        except Exception('')
 
