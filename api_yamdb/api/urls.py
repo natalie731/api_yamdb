bd1626226a57f0b831702126f3b477b792a13194
Дубинин Алексей <clordart@yandex.ru>
2022-08-10 17:55:32 +0300

    Реализация:сериалайзеры, вьюсеты и эндпоинты (роуты) для категорий, жанров и произведений

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import UserViewSet
+from .views import GenreViewSet, CategoryViewSet, TitlesViewSet, UserViewSet
 
 router_v1 = routers.DefaultRouter()
-router_v1.register('signup', UserViewSet)
+router_v1.register('auth/signup', UserViewSet)
+router_v1.register(r'categories', CategoryViewSet, basename='Category')
+router_v1.register(r'genres', GenreViewSet, basename='Genre')
+router_v1.register(r'titles', TitlesViewSet, basename='Title')
+
 
 urlpatterns = [
-    path('v1/auth/', include(router_v1.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
